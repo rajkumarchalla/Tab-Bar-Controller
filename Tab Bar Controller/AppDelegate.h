902f21c764bfe5509a4a2190fe847d76c3bfc684
TEKGLINT SOFTSOL <tekglintsoftsol@TEKGLINTs-iMac.local>
2013-08-13 10:28:45 +0530

    Initial Commit

---
+++ Tab Bar Controller/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tab Bar Controller
+//
+//  Created by TEKGLINT SOFTSOL on 8/13/13.
+//  Copyright (c) 2013 TEKGLINT SOFTSOL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tab Bar Controller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tab Bar Controller
+//
+//  Created by TEKGLINT SOFTSOL on 8/13/13.
+//  Copyright (c) 2013 TEKGLINT SOFTSOL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
